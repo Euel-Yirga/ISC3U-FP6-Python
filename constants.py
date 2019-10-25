8a0f7a9d0e2d655c6e4e6db50079ed95708997fa
Ubuntu <ubuntu@ip-172-31-30-141.ec2.internal>
2019-10-25 17:27:44 +0000

    fp 6

---
+++ constants.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by:Euel Yirga
+# Created on:October 2019
+# This makes sound on the pybadge
+
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_Y = 16
+SCREEN_GRID_X = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+
+# using for button state
+button_state = {
+"button_up": "up",
+"button_just_pressed": "just pressed",
+"button_still_pressed": "still pressed",
+"button_released": "released"
+}
